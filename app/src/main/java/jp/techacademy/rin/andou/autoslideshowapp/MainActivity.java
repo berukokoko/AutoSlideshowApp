2140583a96bb0387974ee78f98ea8596339ff7c4
berukokoko <beru2491@gmail.com>
2018-08-18 19:46:14 +0900

    編集

--- app/src/main/java/jp/techacademy/rin/andou/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/rin/andou/autoslideshowapp/MainActivity.java
@@ -89,20 +89,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    Cursor cursor;
+    Cursor cursor ;
 
     //画像を保存する。
     private void getContentsInfo() {
 
         // 画像の情報を取得する
         ContentResolver resolver = getContentResolver();
-        Cursor cursor = resolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
-                null, // 項目(null = 全項目)
-                null, // フィルタ条件(null = フィルタなし)
-                null, // フィルタ用パラメータ
-                null // ソート (null ソートなし)
-        );
+
+
+            Cursor cursor = resolver.query(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+                    null, // 項目(null = 全項目)
+                    null, // フィルタ条件(null = フィルタなし)
+                    null, // フィルタ用パラメータ
+                    null// ソート (null ソートなし)
+            );
 
         if (cursor.moveToFirst()) {
             do{
